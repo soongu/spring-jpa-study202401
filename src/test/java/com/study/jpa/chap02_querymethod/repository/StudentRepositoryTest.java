8d375be54a5a2e7237dcbb5fc8a0bd5a03447300
soongu <naong526@naver.com>
2024-01-16 09:50:11 +0900

    #4. JPA 페이징 처리

--- src/test/java/com/study/jpa/chap02_querymethod/repository/StudentRepositoryTest.java
+++ src/test/java/com/study/jpa/chap02_querymethod/repository/StudentRepositoryTest.java
@@ -1,12 +1,15 @@
 package com.study.jpa.chap02_querymethod.repository;
 
 import com.study.jpa.chap02_querymethod.entity.Student;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -17,6 +20,32 @@ class StudentRepositoryTest {
 
     @Autowired
     StudentRepository studentRepository;
+
+
+    @BeforeEach
+    void insertData() {
+        Student s1 = Student.builder()
+                .name("쿠로미")
+                .city("청양군")
+                .major("경제학")
+                .build();
+
+        Student s2 = Student.builder()
+                .name("춘식이")
+                .city("서울시")
+                .major("컴퓨터공학")
+                .build();
+
+        Student s3 = Student.builder()
+                .name("어피치")
+                .city("제주도")
+                .major("화학공학")
+                .build();
+
+        studentRepository.save(s1);
+        studentRepository.save(s2);
+        studentRepository.save(s3);
+    }
 
 
     @Test
@@ -31,6 +60,99 @@ class StudentRepositoryTest {
         //when
         studentRepository.save(student);
         //then
+    }
+
+
+
+    @Test
+    @DisplayName("이름이 춘식이인 학생의 정보를 단일조회한다.")
+    void findByNameTest() {
+        //given
+        String name = "춘식이";
+        //when
+        List<Student> studentList = studentRepository.findByName(name);
+        //then
+        assertEquals(1, studentList.size());
+
+        System.out.println("studentList.get(0) = " + studentList.get(0));
+
+
+    }
+
+
+
+    @Test
+    @DisplayName("전공에 공학이 포함된 학생정보를 조회한다.")
+    void findByContainingTest() {
+        //given
+        String majorKeyword = "공학";
+        //when
+        List<Student> studentList = studentRepository.findByMajorContaining(majorKeyword);
+        //then
+        assertEquals(2, studentList.size());
+
+        System.out.println("\n\n\n\n");
+        studentList.forEach(System.out::println);
+        System.out.println("\n\n\n\n");
+    }
+
+
+
+    @Test
+    @DisplayName("네이티브 SQL로 이름으로 조회하기")
+    void nativeSQLTest() {
+        //given
+        String name = "쿠로미";
+        //when
+        Student student = studentRepository.findByNameWithSQL(name);
+        //then
+        assertNotNull(student);
+        assertEquals("청양군", student.getCity());
+    }
+
+
+
+    @Test
+    @DisplayName("JPQL메서드를 사용한 도시이름으로 학생조회")
+    void findByCityWithJPQL() {
+        //given
+        String city = "청양군";
+        //when
+        Student student = studentRepository.getByCityWithJPQL(city);
+        //then
+        assertEquals("쿠로미", student.getName());
+    }
+
+
+
+    @Test
+    @DisplayName("JPQL을 이용한 이름으로 검색하기")
+    void searchNameTest() {
+        //given
+        String name = "춘";
+        //when
+        List<Student> students = studentRepository.searchByNameWithJPQL(name);
+        //then
+        assertEquals(1, students.size());
+
+        System.out.println("students.get(0) = " + students.get(0));
+
+    }
+
+
+
+    @Test
+    @DisplayName("JPQL로 삭제하기")
+    void deleteWithJPQLTest() {
+        //given
+        String name = "어피치";
+        String city = "제주도";
+        //when
+        studentRepository.deleteByNameWithJPQL(name, city);
+
+        //then
+        List<Student> studentList = studentRepository.findByName(name);
+        assertEquals(0, studentList.size());
     }
 
 
---
+++ src/test/java/com/study/jpa/chap03_pagination/repository/StudentPageRepositoryTest.java
@@ -0,0 +1,102 @@
+package com.study.jpa.chap03_pagination.repository;
+
+import com.study.jpa.chap02_querymethod.entity.Student;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+@Rollback
+class StudentPageRepositoryTest {
+
+    @Autowired
+    StudentPageRepository studentPageRepository;
+
+    @BeforeEach
+    void bulkInsert() {
+        // 학생 147명을 저장
+        for (int i = 1; i <= 147; i++) {
+            Student s = Student.builder()
+                    .name("김딱팍" + i)
+                    .city("도시시" + i)
+                    .major("전공공공" + i)
+                    .build();
+
+            studentPageRepository.save(s);
+        }
+    }
+
+
+    @Test
+    @DisplayName("기본적인 페이징 테스트")
+    void basicPageTest() {
+        //given
+        int pageNo = 1;
+        int amount = 10;
+
+        // 페이지 정보 객체 생성 (Pageable)
+        // 여기서는 페이지 번호가 zero-based임 : 1페이지는 0이다
+        Pageable pageInfo = PageRequest.of(
+                pageNo - 1,
+                amount,
+                // Sort.by("name").descending()  // 엔터티 필드명을 써야함
+
+                Sort.by(
+                        Sort.Order.desc("name"),
+                        Sort.Order.asc("city")
+                )
+        );
+
+        //when
+        Page<Student> studentPage = studentPageRepository.findAll(pageInfo);
+
+        // 실질적인 조회데이터 꺼내기
+        List<Student> studentList = studentPage.getContent();
+
+        // 총 페이지 수
+        int totalPages = studentPage.getTotalPages();
+
+        // 총 학생 수
+        long count = studentPage.getTotalElements();
+
+        //then
+        System.out.println("\n\n\n");
+        studentList.forEach(System.out::println);
+        System.out.println("\n\n\n");
+        System.out.println("totalPages = " + totalPages);
+        System.out.println("count = " + count);
+
+    }
+
+    @Test
+    @DisplayName("이름검색 + 페이징")
+    void testSearchAndPagination() {
+        //given
+        int pageNo = 1;
+        int size = 10;
+        Pageable pageInfo = PageRequest.of(pageNo - 1, size);
+        //when
+        Page<Student> students
+                = studentPageRepository.findByNameContaining("3", pageInfo);
+
+        //then
+        System.out.println("\n\n\n");
+        students.getContent().forEach(System.out::println);
+        System.out.println("\n\n\n");
+    }
+
+
+}
